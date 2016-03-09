bdb3082eebe23974cb08ab9e7b24125cf387801b
jwillage <jwillage@gmail.com>
2016-03-08 20:12:41 -0500

    cleanup

--- shots.R
+++ shots.R
@@ -1,7 +1,7 @@
 library(rjson)
-library(ggplot2)
-library(png)
 library(dplyr)
+library(png)
+library(ggplot2)
 
 gameId <- "0923"
 season <- 2015
@@ -19,19 +19,18 @@ url <- paste0("http://stats.nba.com/stats/shotchartdetail?Period=0&VsConference=
               "StartRange=&EndPeriod=&SeasonType=Regular+Season&SeasonSegment=&",
               "GameID=", paste0("00", substr(season, 1, 1), substr(season, 3, 4), "0", gameId))
 raw <- fromJSON(file = url)
-game <- data.frame(matrix(unlist(raw$resultSets[[1]][[3]]), ncol = 21, byrow = TRUE))
-names(game) <- raw$resultSets[[1]][[2]]
+game <-  data.frame(t(sapply(raw$resultSets[[1]]$rowSet, function(x) unlist(x))))
+names(game) <- raw$resultSets[[1]]$headers
 game <- filter(game, TEAM_NAME == team)
 game$LOC_X <- as.numeric(levels(game$LOC_X))[game$LOC_X]
 game$LOC_Y <- as.numeric(levels(game$LOC_Y))[game$LOC_Y]
 
 accuracy <- as.data.frame.matrix(t(table(game$EVENT_TYPE, game$SHOT_ZONE_BASIC)))
 accuracy$pct <- round(accuracy$`Made Shot` / rowSums(accuracy) * 100, 2)
-overall <- round(sum(accuracy$`Made Shot`) / 
-                   (sum(accuracy$`Made Shot`) + sum(accuracy$`Missed Shot`)) * 100, 2)
-
 loc <- game %>% group_by(SHOT_ZONE_BASIC) %>% summarize(x = mean(LOC_X), y = mean(LOC_Y))
 accuracy <- cbind(accuracy, loc[, 2:3])
+overall <- round(sum(accuracy$`Made Shot`) / 
+                   (sum(accuracy$`Made Shot`) + sum(accuracy$`Missed Shot`)) * 100, 2)
 
 gameInfoUrl <- paste0("http://stats.nba.com/stats/boxscoresummaryv2?GameID=",
                       paste0("00", substr(season, 1, 1), substr(season, 3, 4), "0", gameId))
@@ -46,8 +45,7 @@ ggplot(game, aes(x = LOC_X * -1, y = LOC_Y * -1)) +
   inset_raster(readPNG("nba-halfcourt.png"), xmin = -250, xmax = 250, ymin = -420, ymax = 50) +
   geom_point(aes(color = EVENT_TYPE), size = 6, alpha = 0.5) +
   scale_color_manual(labels = c("Make", "Miss"), values = c("GREEN", "RED")) +
-  xlim(-250, 250) +
-  ylim(-420, 50) +
+  lims(x = c(-250, 250), y = c(-420, 50)) +
   coord_fixed() +
   geom_text(data = data.frame(x = (accuracy$x * -1), y = (accuracy$y * -1), pct = accuracy$pct), 
             aes(x = x, y = y, label = pct, fontface = 2), size = 6, color = "gray26") +
@@ -65,5 +63,4 @@ ggplot(game, aes(x = LOC_X * -1, y = LOC_Y * -1)) +
         )  +
   ggtitle(paste(date, away, awayPts, "@", home, homePts, "\n", team, "shots")) + 
   annotate("text", x = 200, y = -405, label = paste(overall, "Overall"), size = 6, fontface = 2)
-
 
